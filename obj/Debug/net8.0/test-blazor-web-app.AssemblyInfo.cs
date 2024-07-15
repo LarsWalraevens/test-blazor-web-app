b76ad6797dae7ba3a16617c8492f773f85805c13
Lars <larswalraevens@live.be>
2024-07-15 12:14:51 +0200

    swap places & empty cells

--- obj/Debug/net8.0/test-blazor-web-app.AssemblyInfo.cs
+++ obj/Debug/net8.0/test-blazor-web-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test-blazor-web-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e62034f171a2fd4851f402762a1f9b8c0f6d81e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0da172011c6282afa58b85af8ca08bd5fb80528a")]
 [assembly: System.Reflection.AssemblyProductAttribute("test-blazor-web-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test-blazor-web-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
