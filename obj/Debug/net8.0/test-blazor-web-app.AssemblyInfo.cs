6a30f9c467e9f8c96c19d7bdc429ab1b7ea7b30c
Lars <larswalraevens@live.be>
2024-08-12 10:37:58 +0200

    timeline control component

--- obj/Debug/net8.0/test-blazor-web-app.AssemblyInfo.cs
+++ obj/Debug/net8.0/test-blazor-web-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test-blazor-web-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d775e3283f38867e0570cd00e9c5f477d621f51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a6d2672b18076099a7c92e7a15114250c4266e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("test-blazor-web-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test-blazor-web-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
